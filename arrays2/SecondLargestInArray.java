11bcd88344ecd314c154b0b4274d18a72fd30ca8
Neeraj Singh <869neerajsingh@gmail.com>
2023-11-28 22:48:14 +0530

    add more ways to find second largest in array

--- arrays2/SecondLargestInArray.java
+++ arrays2/SecondLargestInArray.java
@@ -1,6 +1,7 @@
 package arrays2;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class SecondLargestInArray {
 	
@@ -16,7 +17,9 @@ public class SecondLargestInArray {
 	// Simple approach
 	public static int secondLargestElement1(int[] arr) {
         int n = arr.length;
+        
         Arrays.sort(arr);
+        
         int ans = Integer.MIN_VALUE;
         for(int i = n-2; i>=0; i--){
             if(arr[i] < arr[n-1]){
@@ -24,12 +27,14 @@ public class SecondLargestInArray {
                 break;
             }
         }
+        
         return ans;
     }
 	
 	// Better approach
-	public static int secondLargestElement(int[] arr) {
+	public static int secondLargestElement2(int[] arr) {
         int n = arr.length;
+        
         int largest = Integer.MIN_VALUE, secondLargest = Integer.MIN_VALUE;
         for(int i = 0; i<n; i++){
             if(arr[i] > largest){
@@ -42,12 +47,38 @@ public class SecondLargestInArray {
                 }
             }
         }
+        
         return secondLargest;
     }
+	
+	public static int secondLargestElement3(int[] arr, int n) {
+    	int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, arr[i]);
+        }
 
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != max) {
+                secondMax = Math.max(secondMax, arr[i]);
+            }
+        }
+
+        return secondMax;
+    }
+
+	public static void main(String[] args) 
+	{
+	    Scanner sr = new Scanner(System.in);
+	    int n=sr.nextInt();
+	    int ar[] = new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        ar[i]= sr.nextInt();
+	    }
+	    int ans = secondLargestElement3(ar,n);
+	    System.out.println(ans);
 	}
 
 }
