b9aad66142cb0873b2dfd297c7daef70c4e68a05
Neeraj Singh <869neerajsingh@gmail.com>
2023-12-08 15:43:02 +0530

    feat: solve few dp problems

--- dp/MaxSquareWithAllZeros.java
+++ dp/MaxSquareWithAllZeros.java
@@ -1,5 +1,9 @@
 package dp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MaxSquareWithAllZeros {
 	
 	/*
@@ -75,9 +79,36 @@ public class MaxSquareWithAllZeros {
 		return ans;
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static int[][] takeInput() throws IOException {
 
+        String[] nm;
+        nm = br.readLine().split("\\s");
+        int n = Integer.parseInt(nm[0]);
+        int m = Integer.parseInt(nm[1]);
+		int arr[][]=new int[n][m];
+
+        if (n == 0) {
+            return arr;
+        }
+        
+
+		String[] strNums;
+        for (int i = 0; i < n; ++i) {            
+            strNums = br.readLine().split("\\s");
+            for (int j = 0; j < m; ++j){
+                arr[i][j] = Integer.parseInt(strNums[j]);
+            }
+            
+        }
+
+        return arr;
+    }
+
+	public static void main(String[] args) throws IOException {
+
+        int[][] arr = takeInput();
+		System.out.println(findMaxSquareWithAllZeros2(arr));
 	}
 
 }
--- dp/SubsetSum.java
+++ dp/SubsetSum.java
@@ -1,5 +1,9 @@
 package dp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class SubsetSum {
 	
 	/*
@@ -45,9 +49,33 @@ public class SubsetSum {
         return dp[n][sum];
     }
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int size = Integer.parseInt(br.readLine().trim());
+        int[] input = new int[size];
 
-	}
+        if (size == 0) {
+            System.out.print("No");
+            return;
+        }
+        
+		String[] strNums;
+		strNums = br.readLine().split("\\s");
+		
+        for (int i = 0; i < size; ++i) {
+            input[i] = Integer.parseInt(strNums[i]);
+        }
+
+        int sum = Integer.parseInt(br.readLine().trim());
+        if (isSubsetPresent(input,size,sum)){
+            System.out.print("Yes");
+        }
+        else{
+            System.out.print("No");
+        }
+
+
+    }
 
 }
