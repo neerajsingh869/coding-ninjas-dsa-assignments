59ae1b0a6f105012cc7d4473d95fb40b49a3a076
Neeraj Singh <869neerajsingh@gmail.com>
2024-04-29 23:26:10 +0530

    feat: add new method to find leaders in array

--- test2/LeadersInArray.java
+++ test2/LeadersInArray.java
@@ -6,25 +6,30 @@ import java.util.Scanner;
 public class LeadersInArray {
 	
 	/*
-	 * Given an integer array A of size n. Find and print all the leaders present in the input array. An array element A[i] is called Leader, if all the elements following it (i.e. present at its right) are less than or equal to A[i].
-Print all the leader elements separated by space and in the same order they are present in the input array.
-Input Format :
-Line 1 : Integer n, size of array
-Line 2 : Array A elements (separated by space)
-Output Format :
- leaders of array (separated by space)
-Constraints :
-1 <= n <= 10^6
-Sample Input 1 :
-6
-3 12 34 2 0 -1
-Sample Output 1 :
-34 2 0 -1
-Sample Input 2 :
-5
-13 17 5 4 6
-Sample Output 2 :
-17 6
+	 * Given an integer array A of size n. Find and print all the 
+	 * leaders present in the input array. An array element A[i] 
+	 * is called Leader, if all the elements following it (i.e. 
+	 * present at its right) are less than or equal to A[i].
+		
+		Print all the leader elements separated by space and in 
+		the same order they are present in the input array.
+		Input Format :
+		Line 1 : Integer n, size of array
+		Line 2 : Array A elements (separated by space)
+		Output Format :
+		 leaders of array (separated by space)
+		Constraints :
+		1 <= n <= 10^6
+		Sample Input 1 :
+		6
+		3 12 34 2 0 -1
+		Sample Output 1 :
+		34 2 0 -1
+		Sample Input 2 :
+		5
+		13 17 5 4 6
+		Sample Output 2 :
+		17 6
 	 */
 	
 	// Time complexity -> O(n*n), Space complexity -> O(1)
@@ -71,6 +76,19 @@ Sample Output 2 :
 		}
 		for(int i = arr.size() - 1; i>=0; i--){
 			System.out.print(arr.get(i) + " " );
+		}
+	}
+	
+	// Best approach (Time complexity -> O(n), Space complexity -> O(1))
+	public static void leaders4(int[] input) {
+		int n = input.length;
+		
+		int maxTillNow = Integer.MIN_VALUE;
+		for (int i = n - 1; i >= 0; i--) {
+			if (input[i] > maxTillNow) {
+				System.out.print(input[i] + " ");
+				maxTillNow = input[i];
+			}
 		}
 	}
 
